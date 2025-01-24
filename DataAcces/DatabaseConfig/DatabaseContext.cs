a3151b681c5b87daafc3f2eac35490371b8c9419
LaurensVdb <13851863+LaurensVdb@users.noreply.github.com>
2025-01-24 22:58:55 +0100

    Disable cascade delete when exercise is deleted

--- DataAcces/DatabaseConfig/DatabaseContext.cs
+++ DataAcces/DatabaseConfig/DatabaseContext.cs
@@ -1,33 +1,41 @@
 ﻿using DataAcces.Entities;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataAcces.DatabaseConfig
 {
-    public class DatabaseContext: DbContext
+    public class DatabaseContext : DbContext
     {
         public DbSet<Exercise> Exercises { get; set; }
         public DbSet<WorkoutSet> WorkoutSets { get; set; }
         public DbSet<Workout> Workouts { get; set; }
 
+        /*
+        public DatabaseContext()
+        {
+
+        }
+        */
         public DatabaseContext(DbContextOptions<DatabaseContext> options)
            : base(options)
         {
 
 
         }
-        
-        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<WorkoutSet>()
+           .HasOne(e => e.Exercise)
+           .WithMany(u => u.WorkoutSets)
+           .OnDelete(DeleteBehavior.Restrict);
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite();
             base.OnConfiguring(optionsBuilder);
         }
 
-       
+
     }
 }
---
+++ DataAcces/Migrations/20250124214913_NoCascadeDelete.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAcces.Migrations
+{
+    /// <inheritdoc />
+    public partial class NoCascadeDelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_WorkoutSets_Exercises_ExerciseId",
+                table: "WorkoutSets");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WorkoutSets_Exercises_ExerciseId",
+                table: "WorkoutSets",
+                column: "ExerciseId",
+                principalTable: "Exercises",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_WorkoutSets_Exercises_ExerciseId",
+                table: "WorkoutSets");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WorkoutSets_Exercises_ExerciseId",
+                table: "WorkoutSets",
+                column: "ExerciseId",
+                principalTable: "Exercises",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
